df6a758af0ac6d3339971832c96dde8465715c7c
resister-boy <hassanpumped17@gmail.com>
2024-07-12 13:39:03 +0900

    feat: add test pages

---
+++ src/app/test/Client.tsx
@@ -0,0 +1,16 @@
+"use client"
+import React from 'react'
+import styles from '@/app/test/page.module.scss'
+import className from 'classnames/bind'
+
+const cn = className.bind(styles)
+
+const TestClientPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TestClientPage
---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TestClientPage from '@/app/test/Client'
+
+const TestPage = () => {
+  return (
+    <TestClientPage />
+  )
+}
+
+export default TestPage
