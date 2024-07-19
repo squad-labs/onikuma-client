462b74165b70d4563e44c1a637892e8a468086bd
resister-boy <hassanpumped17@gmail.com>
2024-07-19 16:51:28 +0900

    types: declare button types

---
+++ src/shared/types/ui/Button.ts
@@ -0,0 +1,31 @@
+import { ButtonHTMLAttributes, ReactNode } from 'react';
+
+export type ButtonShapeType =
+  | 'shape-1'
+  | 'shape-2'
+  | 'shape-3'
+  | 'shape-4'
+  | 'shape-5'
+  | 'shape-6';
+
+export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  name: string;
+  disabled?: boolean;
+  shape: ButtonShapeType;
+  onClick: () => void;
+  fontSize?: 'small' | 'medium' | 'large';
+  fontWeight?: 'light' | 'regular' | 'bold';
+  loading?: boolean;
+  children?: ReactNode;
+  classNames?: string[];
+};
+
+export type PaddingButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  name: string;
+  disabled?: boolean;
+  height: 'small' | 'medium' | 'large';
+  fontSize?: 'small' | 'medium' | 'large';
+  fontWeight?: 'light' | 'regular' | 'bold';
+  children?: ReactNode;
+  classNames?: string[];
+}
