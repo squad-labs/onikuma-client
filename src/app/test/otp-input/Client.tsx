20b8d394b518599d09b97dcda3554babecbbe332
Ruby <rubycho97@gmail.com>
2024-07-28 20:13:04 -0700

    fix: push to add unsaved file

---
+++ src/app/test/otp-input/Client.tsx
@@ -0,0 +1,35 @@
+'use client';
+import React from 'react';
+import styles from '@/app/test/otp-input/page.module.scss';
+import className from 'classnames/bind';
+import OTPInput from '@/widgets/inputs/otpInput';
+
+const cn = className.bind(styles);
+
+const TestClientPage = () => {
+
+    const [otpValues, setOtpValues] = React.useState(['', '', '', '', '']);
+
+    const handleOtpChange = (index: number, value: string) => {
+        const newOtpValues = [...otpValues];
+        newOtpValues[index] = value;
+        setOtpValues(newOtpValues);
+    };
+
+  return (
+    <div className={cn('container')}>
+      <h2>OTP Input</h2>
+      <section className={cn('element-container')}>
+        <OTPInput
+            name = "OTP"
+            values = {otpValues}
+            onChange = {handleOtpChange}
+        />
+      </section>
+      <section className={cn('element-container')}>
+      </section>
+    </div>
+  );
+};
+
+export default TestClientPage;
