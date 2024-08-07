bfae249fc60fe34bf7d3149c9d590a681235a19d
resister-boy <hassanpumped17@gmail.com>
2024-08-07 17:47:31 +0900

    ui: add base logo widget

---
+++ src/widgets/logo/baseLogo/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from '@/widgets/logo/baseLogo/BaseLogo.module.scss';
+import classNames from 'classnames/bind';
+import TextLogo from '@/assets/logo/text-logo.svg'
+import { BaseLogoProps } from '@/shared/types/ui/Logo';
+
+const cn = classNames.bind(styles);
+
+const BaseLogo = ({ size }: BaseLogoProps) => {
+  return (
+    <TextLogo viewBox="0 0 128 40" className={cn('logo', `logo-${size}`)} />
+  )
+}
+
+export default BaseLogo
