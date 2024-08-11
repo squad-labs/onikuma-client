1e4a20ec0d7cb0d16dc78d4cac8cedb7112e1756
resister-boy <hassanpumped17@gmail.com>
2024-08-12 01:08:03 +0900

    config: declare base metadata

---
+++ src/shared/constants/METADATA.ts
@@ -0,0 +1,17 @@
+export const BASE_METADATA = {
+  title: 'Onikuma',
+  siteName: 'Onikuma | On-Chain Social Game',
+  description: 'Onikuma, On-Chain Social Game on berachain. Bet on what you believe.',
+  keywords: [
+    'Onikuma',
+    'Crypto',
+    'Onchain',
+    'Blockchain',
+    'Social',
+    'Game',
+    'Berachain',
+  ],
+  url: 'https://onikuma.world',
+  image: 'https://dev-onikuma-s3.s3.ap-northeast-2.amazonaws.com/Hero.png',
+  ogImage: '/metadata/og-image.png',
+}
