b2c287b6063664b9e7bd28bb3ce06120315fa129
Ruby <rubycho97@gmail.com>
2024-08-13 01:47:33 -0700

    feat: develops base toast

--- src/shared/types/ui/Toast.ts
+++ src/shared/types/ui/Toast.ts
@@ -1,8 +1,10 @@
 import { ReactNode } from 'react';
 
-export type ToastProps = {
+export type BaseToastProps = {
   type?: 'success' | 'error' | 'requireAction';
   message?: string;
+  secondaryMessage?: string;
+  closable?: boolean;
   onClose?: () => void;
   children?: ReactNode;
   classNames?: string[];
