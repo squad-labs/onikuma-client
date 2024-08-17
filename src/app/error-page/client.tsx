ca4f7de5e82f32a5ed6bad422e515b771882a241
Ruby <rubycho97@gmail.com>
2024-08-16 19:25:47 -0700

    feat: adds text and image to page

---
+++ src/app/error-page/client.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '@/app/error-page/client.module.scss';
+import classNames from 'classnames/bind';
+
+const cn = classNames.bind(styles);
+
+const ErrorPage = () => {
+  return <div></div>;
+};
+
+export default ErrorPage;
