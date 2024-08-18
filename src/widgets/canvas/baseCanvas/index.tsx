35f8ddf242d73ac870e4cb9a371a20eaaf85d2be
resister-boy <hassanpumped17@gmail.com>
2024-08-18 22:41:42 +0900

    ui: add base canvas widget

---
+++ src/widgets/canvas/baseCanvas/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from '@/widgets/canvas/baseCanvas/BaseCanvas.module.scss'
+import classNames from 'classnames/bind'
+
+const cn = classNames.bind(styles)
+
+const BaseCanvas = () => {
+  return (
+    <canvas>
+
+    </canvas>
+  )
+}
+
+export default BaseCanvas
