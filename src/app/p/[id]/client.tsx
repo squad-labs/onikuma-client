e2e1ff5d0e83d6994607e51750b800360ffcb3fe
resister-boy <hassanpumped17@gmail.com>
2024-08-18 22:44:24 +0900

    chore

--- src/app/p/[id]/client.tsx
+++ src/app/p/[id]/client.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import styles from '@/app/p/[id]/client.module.scss'
 import classNames from 'classnames/bind'
+import DateText from '@/widgets/text/dateText'
+import BaseDivider from '@/widgets/divider/baseDivider'
+import { COLOR } from '@/shared/constants/COLOR'
 
 const cn = classNames.bind(styles)
 
@@ -9,9 +12,18 @@ type Props = {
 }
 
 const PlayClientPage = ({ id }: Props) => {
+  
   return (
     <div>
-      <span>{id}</span>
+      <BaseDivider
+        type='vertical'
+        color={COLOR.DARK_GRAY_2}
+        length={100}
+        minLength={20}
+        thick={2}
+        radius={2}
+      />
+      <DateText startDate='2024-08-20' />
     </div>
   )
 }
