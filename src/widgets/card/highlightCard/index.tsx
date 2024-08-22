da304d1fdd56facafcda1b0c107dbb7d0432e761
Ruby <rubycho97@gmail.com>
2024-08-21 19:41:21 -0700

    ui: creates highlight card widget and implements it in page

---
+++ src/widgets/card/highlightCard/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from '@/widgets/card/highlightCard/HighlightCard.module.scss';
+import classNames from 'classnames/bind';
+
+const cn = classNames.bind(styles);
+
+type HighlightCardProps = {
+  title: string;
+  mainText: string;
+  subText: string;
+};
+
+const HighlightCard = ({
+  title,
+  mainText,
+  subText,
+}: HighlightCardProps) => {
+  return (
+    <div className={cn('card')}>
+      <p className={cn('title')}>
+        {title}
+      </p>
+      <div className={cn('non-title')}>
+        <p className={cn('main-text')}>
+          {mainText}
+        </p>
+        <p className={cn('sub-text')}>
+          {subText}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HighlightCard;
+
