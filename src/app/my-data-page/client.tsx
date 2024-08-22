6b5f709bdf5b8e03fa1c42ec2f57808ead979500
Ruby <rubycho97@gmail.com>
2024-08-21 20:48:24 -0700

    ui: edits page css, yarn prettier

--- src/app/my-data-page/client.tsx
+++ src/app/my-data-page/client.tsx
@@ -7,12 +7,60 @@ import MyDataTable from '@/widgets/table/myDataTable';
 const cn = classNames.bind(styles);
 
 const mySampleData = [
-  { name: 'Who is the best kisser?', status: 'Ongoing', schedule: '2024-08-20 - 2024-08-27', pooledIn: '$287,000', pnlDollars: '+287,000', pnlPercent: '+25.25%', checkResults: 'Check Results' },
-  { name: 'Who will you vote for the next election?', status: 'Ongoing', schedule: '2024-08-20 - 2024-08-27', pooledIn: '$287,000', pnlDollars: '+287,000', pnlPercent: '+25.25%', checkResults: 'Hall of Honor' },
-  { name: 'Your favorite anime character?', status: 'Ended', schedule: '2024-08-20 - 2024-08-27', pooledIn: '$287,000', pnlDollars: '+287,000', pnlPercent: '+25.25%', checkResults: 'Check Results' },
-  { name: 'Item name', status: 'Ended', schedule: '2024-08-20 - 2024-08-27', pooledIn: '$287,000', pnlDollars: '+287,000', pnlPercent: '+25.25%', checkResults: 'Check Results' },
-  { name: 'Item name', status: 'Ended', schedule: '2024-08-20 - 2024-08-27', pooledIn: '$287,000', pnlDollars: '+287,000', pnlPercent: '+25.25%', checkResults: 'Check Results' },
-  { name: 'Item name', status: 'Ended', schedule: '2024-08-20 - 2024-08-27', pooledIn: '$287,000', pnlDollars: '+287,000', pnlPercent: '+25.25%', checkResults: 'Check Results' }
+  {
+    name: 'Who is the best kisser?',
+    status: 'Ongoing',
+    schedule: '2024-08-20 - 2024-08-27',
+    pooledIn: '$287,000',
+    pnlDollars: '+287,000',
+    pnlPercent: '+25.25%',
+    checkResults: 'Check Results',
+  },
+  {
+    name: 'Who will you vote for the next election?',
+    status: 'Ongoing',
+    schedule: '2024-08-20 - 2024-08-27',
+    pooledIn: '$287,000',
+    pnlDollars: '+287,000',
+    pnlPercent: '+25.25%',
+    checkResults: 'Hall of Honor',
+  },
+  {
+    name: 'Your favorite anime character?',
+    status: 'Ended',
+    schedule: '2024-08-20 - 2024-08-27',
+    pooledIn: '$287,000',
+    pnlDollars: '+287,000',
+    pnlPercent: '+25.25%',
+    checkResults: 'Check Results',
+  },
+  {
+    name: 'Item name',
+    status: 'Ended',
+    schedule: '2024-08-20 - 2024-08-27',
+    pooledIn: '$287,000',
+    pnlDollars: '+287,000',
+    pnlPercent: '+25.25%',
+    checkResults: 'Check Results',
+  },
+  {
+    name: 'Item name',
+    status: 'Ended',
+    schedule: '2024-08-20 - 2024-08-27',
+    pooledIn: '$287,000',
+    pnlDollars: '+287,000',
+    pnlPercent: '+25.25%',
+    checkResults: 'Check Results',
+  },
+  {
+    name: 'Item name',
+    status: 'Ended',
+    schedule: '2024-08-20 - 2024-08-27',
+    pooledIn: '$287,000',
+    pnlDollars: '+287,000',
+    pnlPercent: '+25.25%',
+    checkResults: 'Check Results',
+  },
 ];
 
 const MyClientPage = () => {
@@ -22,26 +70,24 @@ const MyClientPage = () => {
       <div className={cn('data-container')}>
         <div className={cn('highlights-container')}>
           <HighlightCard
-            title = "Total Pooled in Amount" 
-            mainText = "$1,423.23"
-            subText = "0%"/>
+            title="Total Pooled in Amount"
+            mainText="$1,423.23"
+            subText="0%"
+          />
           <HighlightCard
-            title = "Total Earnings / Loss" 
-            mainText = "+$1,423.23"
-            subText = "0%"/>
+            title="Total Earnings / Loss"
+            mainText="+$1,423.23"
+            subText="0%"
+          />
+          <HighlightCard title="PnL (%)" mainText="+32%" subText="0%" />
           <HighlightCard
-            title = "PnL (%)" 
-            mainText = "+32%"
-            subText = "0%"/>
-          <HighlightCard
-            title = "Base Asset" 
-            mainText = "$HONEY"
-            subText = "BERACHAIN"/>
+            title="Base Asset"
+            mainText="$HONEY"
+            subText="BERACHAIN"
+          />
         </div>
         <div className={cn('my-data-table-container')}>
-          <MyDataTable
-            data = {mySampleData}
-          />
+          <MyDataTable data={mySampleData} />
         </div>
       </div>
     </div>
--- src/widgets/table/myDataTable/index.tsx
+++ src/widgets/table/myDataTable/index.tsx
@@ -5,52 +5,52 @@ import classNames from 'classnames/bind';
 const cn = classNames.bind(styles);
 
 type TableRow = {
-    name: string;
-    status: string;
-    schedule: string;
-    pooledIn: string;
-    pnlDollars: string;
-    pnlPercent: string;
-    checkResults: string;
-}
-
-type MyDataTableProps = {
-    data: TableRow[];
+  name: string;
+  status: string;
+  schedule: string;
+  pooledIn: string;
+  pnlDollars: string;
+  pnlPercent: string;
+  checkResults: string;
 };
 
-const MyDataTable = ({
-    data
-}:MyDataTableProps) => {
-    return (
-        <table className={cn('my-table')}>
-            <thead>
-                <tr className={cn('table-row-names')}>
-                    <th className={cn('th-name')}>Name</th>
-                    <th>Status</th>
-                    <th>Schedule</th>
-                    <th>Pooled in</th>
-                    <th>PnL ($)</th>
-                    <th>PnL (%)</th>
-                    <th>Check Results</th>
-                </tr>
-            </thead>
-            <tbody>
-                {data.map((item,index) => (
-                    <tr key={index}>
-                        <td className={cn('td-name')}>{item.name}</td>
-                        <td><button>{item.status}</button></td>
-                        <td>{item.schedule}</td>
-                        <td>{item.pooledIn}</td>
-                        <td>{item.pnlDollars}</td>
-                        <td>{item.pnlPercent}</td>
-                        <td><button>{item.checkResults}</button></td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    );
+type MyDataTableProps = {
+  data: TableRow[];
+};
+
+const MyDataTable = ({ data }: MyDataTableProps) => {
+  return (
+    <table className={cn('my-table')}>
+      <thead>
+        <tr className={cn('table-row-names')}>
+          <th className={cn('th-name')}>Name</th>
+          <th>Status</th>
+          <th>Schedule</th>
+          <th>Pooled in</th>
+          <th>PnL ($)</th>
+          <th>PnL (%)</th>
+          <th>Check Results</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, index) => (
+          <tr key={index}>
+            <td className={cn('td-name')}>{item.name}</td>
+            <td>
+              <button>{item.status}</button>
+            </td>
+            <td>{item.schedule}</td>
+            <td>{item.pooledIn}</td>
+            <td>{item.pnlDollars}</td>
+            <td>{item.pnlPercent}</td>
+            <td>
+              <button>{item.checkResults}</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default MyDataTable;
-
-
