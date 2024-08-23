91d58f994c51fab1247fa54515bfd284c48177af
Ruby <rubycho97@gmail.com>
2024-08-22 21:20:57 -0700

    ui: adds button and status bar

---
+++ src/widgets/bar/statusBar/index.tsx
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+import styles from '@/widgets/bar/statusBar/StatusBar.module.scss';
+import classNames from 'classnames/bind';
+import { StatusBarProps} from '@/shared/types/ui/Bar';
+
+const cn = classNames.bind(styles);
+
+const BaseBar = ({
+  label,
+  classNames = [],
+}: StatusBarProps) => {
+  return (
+    <div className={cn('bar-container', `bar-${label}`, ...classNames)}>
+        <p>{label}</p>
+    </div>
+  );
+};
+
+export default BaseBar;
