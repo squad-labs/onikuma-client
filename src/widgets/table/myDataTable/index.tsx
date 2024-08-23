2fae7508961cd07f50ef4556fd2989cedbebc698
Ruby <rubycho97@gmail.com>
2024-08-22 21:20:57 -0700

    ui: adds button and status bar

--- src/widgets/table/myDataTable/index.tsx
+++ src/widgets/table/myDataTable/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from '@/widgets/table/myDataTable/MyDataTable.module.scss';
 import classNames from 'classnames/bind';
+import CheckResultsButton from '@/components/common/button/checkResultsButton';
+import StatusBar from '@/widgets/bar/statusBar';
 
 const cn = classNames.bind(styles);
 
@@ -33,18 +35,23 @@ const MyDataTable = ({ data }: MyDataTableProps) => {
         </tr>
       </thead>
       <tbody>
-        {data.map((item, index) => (
+        {data.map((item, index) => (   
           <tr key={index}>
             <td className={cn('td-name')}>{item.name}</td>
             <td>
-              <button>{item.status}</button>
+              <StatusBar
+                label = {item.status}
+                />
             </td>
             <td>{item.schedule}</td>
             <td>{item.pooledIn}</td>
             <td>{item.pnlDollars}</td>
             <td>{item.pnlPercent}</td>
-            <td>
-              <button>{item.checkResults}</button>
+            <td className={cn('td-check-results')}>
+              <CheckResultsButton
+                text = {item.checkResults}
+                primaryColor = {item.checkResults.toLowerCase() === 'Hall of Honor' ? 'BASE_RED_1' : item.status.toLowerCase() === 'Ongoing' ? 'BASE_GRAY_1' : 'BASE_BLUE_1'}
+                secondaryColor= 'LIGHT' />
             </td>
           </tr>
         ))}
