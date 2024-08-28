3e4da2a54d0d2c6efd48496de31a7fa4506c0c75
resister-boy <hassanpumped17@gmail.com>
2024-08-29 01:20:18 +0900

    a

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,10 @@
 import { getMetadata } from '@/shared/utils/metadata';
+import styles from '@/app/not-found.module.scss'
+import classNames from 'classnames/bind';
 import { ResolvingMetadata } from 'next';
 import React from 'react';
+import GoHomeButton from '@/widgets/button/paddingButton';
+import ErrorSvg from '@/assets/images/error-image.svg';
 
 type MetadataProps = {
   params: {
@@ -10,6 +14,8 @@ type MetadataProps = {
     [key: string]: string | string[] | undefined;
   };
 };
+
+const cn = classNames.bind(styles);
 
 export const generateMetadata = async (
   { params, searchParams }: MetadataProps,
@@ -21,8 +27,32 @@ export const generateMetadata = async (
 const NotFound = () => {
   return (
     <main>
-      <h1>404</h1>
-      <p>Page not found</p>
+    <div className={cn('page-container')}>
+      <div className={cn('text-container')}>
+        <h1>
+          Oops... <br /> Page not found
+        </h1>
+        <p>This page doesn't exist or was removed!</p>
+        <p>We suggest you back to home.</p>
+        <div className={cn('home-button-container')}>
+          <GoHomeButton
+            name="Go Back Home"
+            text="Go Back Home"
+            label="BackHome Button"
+            height="medium"
+            fontSize="large"
+            fontWeight="regular"
+            onClick={() => console.log('Button Clicked')}
+            classNames={['button-blue']}
+          />
+        </div>
+      </div>
+      <div className={cn('image-container')}>
+        <ErrorSvg
+          className={cn('image')}
+        />
+      </div>
+    </div>
     </main>
   );
 };
