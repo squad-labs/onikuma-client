23bf97e378ca1dca0f5e4a64c073ba262e6a373b
resister-boy <hassanpumped17@gmail.com>
2024-09-09 15:27:06 +0900

    a

--- src/app/api/result/route.ts
+++ src/app/api/result/route.ts
@@ -1,16 +1,23 @@
-import { postShareImage } from '@/shared/api/Image';
 import { generateResultImage } from '@/shared/utils/canvas';
+import axios from 'axios';
 import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const params = await req.json();
   const image = await generateResultImage(params);
 
-  const res = await postShareImage({
-    topicId: params.topicId,
-    file: new Blob([image]),
-    token: params.token,
-  });
+  const formData = new FormData();
+  formData.append('file', new Blob([image]));
+
+  const res = await axios.post(
+    `${process.env.API_BASE_URL}/api/topics/share-image/${params.topicId}`,
+    formData,
+    {
+      headers: {
+        Authorization: `Bearer ${params.token}`,
+      },
+    },
+  );
 
   return new NextResponse(JSON.stringify(res));
 }
