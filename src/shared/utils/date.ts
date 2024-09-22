8a735925a1a6c5a931bac866705348614a8ddfa6
resister-boy <hassanpumped17@gmail.com>
2024-09-22 11:46:12 +0900

    fix: edit challenge date

--- src/shared/utils/date.ts
+++ src/shared/utils/date.ts
@@ -50,16 +50,16 @@ export const fetchTimeFormat = (date: string) => {
 };
 
 export const monthTextMap = {
-  '00': 'January',
-  '01': 'February',
-  '02': 'March',
-  '03': 'April',
-  '04': 'May',
-  '05': 'June',
-  '06': 'July',
-  '07': 'August',
-  '08': 'September',
-  '09': 'October',
-  '10': 'November',
-  '11': 'December',
+  '01': 'January',
+  '02': 'February',
+  '03': 'March',
+  '04': 'April',
+  '05': 'May',
+  '06': 'June',
+  '07': 'July',
+  '08': 'August',
+  '09': 'September',
+  '10': 'October',
+  '11': 'November',
+  '12': 'December',
 };
